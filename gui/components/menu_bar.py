9dff7685be3a12011ca89b899912de2624fc04e6
kalleculated <pascal.kisin@gmail.com>
2024-05-10 00:18:27 +0200

    updated some callback names

--- gui/components/menu_bar.py
+++ gui/components/menu_bar.py
@@ -1,16 +1,16 @@
 import tkinter as tk
 
-def setup_menu(master, file_callback, cross_corr_callback):
+def setup_menu(master, load_signals_callback, example_plot_callback):
     menu_bar = tk.Menu(master)
 
     # Add "File" menu
     file_menu = tk.Menu(menu_bar, tearoff=0)
     menu_bar.add_cascade(label="File", menu=file_menu)
-    file_menu.add_command(label="Open", command=file_callback)
+    file_menu.add_command(label="Open", command=load_signals_callback)
 
     # Add calculation menu
     calculation_menu = tk.Menu(menu_bar, tearoff=0)
     menu_bar.add_cascade(label="Berechnung", menu=calculation_menu)
-    calculation_menu.add_command(label="Kreuzkorrelation", command=cross_corr_callback)
+    calculation_menu.add_command(label="Kreuzkorrelation", command=example_plot_callback)
 
     return menu_bar
