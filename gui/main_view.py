8a570f4a9ded57199f44730b1ea41f054c328e52
kalleculated <pascal.kisin@gmail.com>
2024-05-17 14:37:42 +0200

    added some further exampels

--- gui/main_view.py
+++ gui/main_view.py
@@ -4,22 +4,33 @@ import analysis.signal_processing as sp
 import matplotlib.pyplot as plt
 import gui.components.upload_button as ub
 import gui.components.file_input as fi
+import holoviews as hv
+import panel as pn
+
+hv.extension("bokeh", "plotly")
+
 
 class MainView:
     def __init__(self):
         pn.extension(sizing_mode="stretch_width", template="fast", theme="dark")
+        pn.extension("plotly")
+        pn.extension('floatpanel')
         self.file_input = fi.FileInputComponent()
+        self.sidebar_upload = pn.Column(self.file_input.component, sizing_mode='stretch_width')
         self.file_input.component.param.watch(self.update_main, 'value')
+        self.file_input.component.param.watch(self.update_side, 'value')
+        self.accordion = pn.Accordion(('Upload', self.file_input.component),('Autokorrelation', "test"), sizing_mode='stretch_width')
+        self.tabs = pn.Tabs(('Upload', self.file_input.component),('Autokorrelation', "test"), sizing_mode='stretch_width', dynamic=True)
+        self.floatpanel = pn.layout.FloatPanel(self.tabs, name='Basic FloatPanel', margin=20, contained = False)
 
-        # Erstelle den Seitenbereich mit dem Datei-Input-Widget
+        # Create a sidebar and main area
         self.sidebar = pn.Column(
-            self.file_input.component,
-            sizing_mode='stretch_width'
+            "", sizing_mode='stretch_width'
         )
 
-        self.main = pn.Column("Dashboard", sizing_mode='stretch_width')
+        self.main = pn.Column("", sizing_mode='stretch_width')
 
-        # Setze einige zusätzliche Design- und Template-Parameter
+        # Set some template parameters
         pn.state.template.param.update(
             site="FFT-Analysator",
             title="",
@@ -27,20 +38,48 @@ class MainView:
             accent_base_color="#E91E63",
         )
 
+        # Update the main area
         self.update_main()
+
+        # Update the sidebar area
+        self.update_side()
 
     def update_main(self, event=None):
         if self.file_input.component.value:
             file_data = self.file_input.component.value
             data = sp.count_channels(file_data)
+
+            # Create a holoviews plot with data
+            fig = hv.Curve(data[:,0], kdims="Frequenz", vdims="Amplitude").opts(
+                height=400
+            )
+
             self.main.objects = [
-                pn.pane.Markdown("Willkommen beim FFT-Analysator"),
-                pn.pane.Markdown(f"Anzahl der Kanäle: {data.shape}")
+                pn.pane.HoloViews(fig, sizing_mode="stretch_width"),
+                self.floatpanel
             ]
         else:
-            self.main.objects = [pn.pane.Markdown("Dashboard")]
+            self.main.objects = [pn.pane.Markdown("Keine Datei ausgewählt!"),
+                                 self.floatpanel
+            ]
+
+    def update_side(self, event=None):
+        if self.file_input.component.value:
+            file_data = self.file_input.component.value
+            data = sp.count_channels(file_data)
+
+            self.sidebar.objects = [
+                self.accordion,
+                self.tabs,
+                pn.pane.Markdown(f"Fileshape: {data.shape}")
+            ]
+        else:
+            self.sidebar.objects = [
+                self.accordion,
+                self.tabs
+            ]
 
     def servable(self):
-        # Markiere nur die Haupt- und Seitenbereiche als servable
+        # Mark main and sidebar as servable
         self.sidebar.servable(target="sidebar")
         self.main.servable(target="main")
