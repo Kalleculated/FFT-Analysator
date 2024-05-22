179a7094516ce3340493f8c90ba039cb01b60f17
Al-Qasir, Ramz <ramz.al-qasir@siemens.com>
2024-05-22 14:46:40 +0200

    Changed and expanded generate.py and signal_processing.py

--- analysis/signal_processing.py
+++ analysis/signal_processing.py
@@ -1,12 +1,34 @@
 import h5py
 import io
+from pathlib import Path
+import numpy as np
+import matplotlib.pyplot as plt
 
-def count_channels(file_data):
+def get_channel_data(h5_file, channel):
+    with h5py.File(h5_file, 'r') as file:
+        channel_data = np.array(file['time_data'][:])[:, channel]
+    return channel_data
+def get_channel_size(h5_file, channel):
+    with h5py.File(h5_file, 'r') as file:
+        size = np.array(file['time_data'][channel])
+    return size
+
+def count_channels(h5_file):
     # Erstelle ein file-like object aus den Bytes
-    #print(io.BytesIO(file_data))
-    with h5py.File(io.BytesIO(file_data[0]), 'r') as file:
+    with h5py.File(h5_file, 'r') as file:
         # Zugriff auf den gewünschten Datensatz
-        data = file['time_data'][:]
+        count = np.array(file['time_data'][:]).shape[1]
         # Die Anzahl der Kanäle (zweite Dimension der Daten)
+    return count
 
-    return data
+
+data_directory = Path.joinpath(Path().absolute().parent, "test_data")
+file_path = Path.joinpath(data_directory, "three_sources.h5")
+
+channel_nr = count_channels(file_path)
+channel_info = get_channel_data(file_path, 1)
+channel_size = get_channel_size(file_path, 1)
+
+print(channel_info)
+print(channel_size)
+print(channel_info.shape)
