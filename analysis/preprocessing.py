447a33a994cb906a8a81143353b96adfb7f6f5b1
Al-Qasir, Ramz <ramz.al-qasir@siemens.com>
2024-05-22 15:26:05 +0200

    Added Classes for preprocessing.py and signal_processing.py

--- analysis/preprocessing.py
+++ analysis/preprocessing.py
@@ -1,32 +1,29 @@
 #Script für das Preprocessing. Beispiel Auswahl der Kanäle oder ähnliches.
 import h5py
-import io
 from pathlib import Path
 import numpy as np
-import matplotlib.pyplot as plt
 
-def get_channel_data(h5_file, channel):
-    with h5py.File(h5_file, 'r') as file:
-        channel_data = np.array(file['time_data'][:])[:, channel]
-    return channel_data
-def get_channel_size(h5_file, channel):
-    with h5py.File(h5_file, 'r') as file:
-        size = np.array(file['time_data'][:])[:, channel].shape[0]
-    return size
+class Preprocess:
 
-def count_channels(h5_file):
-    # Erstelle ein file-like object aus den Bytes
-    with h5py.File(h5_file, 'r') as file:
-        # Zugriff auf den gewünschten Datensatz
-        count = np.array(file['time_data'][:]).shape[1]
-        # Die Anzahl der Kanäle (zweite Dimension der Daten)
-    return count
+    def __init__(self,file_path):
 
-
-data_directory = Path.joinpath(Path().absolute().parent, "test_data")
-file_path = Path.joinpath(data_directory, "three_sources.h5")
-
-channel_nr = count_channels(file_path)
-channel_info = get_channel_data(file_path, 0)
-channel_size = get_channel_size(file_path, 0)
-
+        self.current_file = file_path
+        self.channel_count = None
+        self.channel_size = None
+        self.selected_channel_data = None
+        self.data = None
+    def get_channel_data(self, channel):
+        with h5py.File(self.current_file, 'r') as file:
+            channel_data = np.array(file['time_data'][:])[:, channel]
+        return channel_data
+    def get_channel_size(self, channel):
+        with h5py.File(self.current_file, 'r') as file:
+            size = np.array(file['time_data'][:])[:, channel].shape[0]
+        return size
+    def count_channels(self):
+        # Erstelle ein file-like object aus den Bytes
+        with h5py.File(self.current_file, 'r') as file:
+            # Zugriff auf den gewünschten Datensatz
+            count = np.array(file['time_data'][:]).shape[1]
+            # Die Anzahl der Kanäle (zweite Dimension der Daten)
+        return count
