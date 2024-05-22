c60581af2b065e33674438d58d49b642153cfc63
Al-Qasir, Ramz <ramz.al-qasir@siemens.com>
2024-05-22 17:31:14 +0200

    Added an empty abtastrate function

--- analysis/preprocessing.py
+++ analysis/preprocessing.py
@@ -1,6 +1,5 @@
 #Script für das Preprocessing. Beispiel Auswahl der Kanäle oder ähnliches.
 import h5py
-from pathlib import Path
 import numpy as np
 
 class Preprocess:
@@ -12,18 +11,26 @@ class Preprocess:
         self.channel_size = None
         self.selected_channel_data = None
         self.data = None
+
     def get_channel_data(self, channel):
         with h5py.File(self.current_file, 'r') as file:
-            channel_data = np.array(file['time_data'][:])[:, channel]
-        return channel_data
+            data = np.array(file['time_data'][:])[:, channel]
+        return data
+
     def get_channel_size(self, channel):
         with h5py.File(self.current_file, 'r') as file:
             size = np.array(file['time_data'][:])[:, channel].shape[0]
         return size
-    def count_channels(self):
+
+    def get_channel_count(self):
         # Erstelle ein file-like object aus den Bytes
         with h5py.File(self.current_file, 'r') as file:
             # Zugriff auf den gewünschten Datensatz
             count = np.array(file['time_data'][:]).shape[1]
             # Die Anzahl der Kanäle (zweite Dimension der Daten)
         return count
+
+    def get_abtastrate(self):
+        # Muss noch befüllt werden. Vermutlich bietet Acourlar hier eine Funktion oder es kann aus dem Datenset gelesen
+        # gelesen werden
+        return None
