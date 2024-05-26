d6092192c958b9c4571e43e966be0477fb7d139a
kalleculated <pascal.kisin@gmail.com>
2024-05-26 14:34:48 +0200

    added plot to already existing tab

--- gui/views/main_view.py
+++ gui/views/main_view.py
@@ -6,7 +6,9 @@ from panel.pane import HoloViews
 
 class MainView:
     def __init__(self):
-        self.tabs = pn.Tabs(sizing_mode='stretch_width', dynamic=True)
+        self.str_signal_tab = "Signalinput"
+
+        self.tabs = pn.Tabs((self.str_signal_tab, 'Keine Datei ausgewählt!'), sizing_mode='stretch_width', dynamic=True)
 
         self.main = pn.Column(self.tabs, sizing_mode='stretch_width')
 
@@ -16,7 +18,7 @@ class MainView:
         fig = hv.Curve((np.linspace(0,1,51200), data[:,1]), 
                        kdims="Zeit in Sekunden", vdims="Amplitude").opts()
         plot_pane = HoloViews(fig, sizing_mode='stretch_width')
-        self.tabs.append(('Signalinput', pn.Column(plot_pane, sizing_mode='stretch_width')))
+        self.tabs[0] = (self.str_signal_tab, pn.Column(plot_pane, sizing_mode='stretch_width'))
 
     def servable(self):
         self.main.servable(target="main")
