b39892367dcda160bee066c678131a294de8f3cf
kalleculated <pascal.kisin@gmail.com>
2024-05-26 13:14:59 +0200

    readded tabs for main_view

--- gui/views/main_view.py
+++ gui/views/main_view.py
@@ -2,7 +2,9 @@ import panel as pn
 
 class MainView:
     def __init__(self):
-        self.main = pn.Column("", sizing_mode='stretch_width')
+        self.tabs = pn.Tabs(('Signalinput',"test"),('Ergebnis', "test"), sizing_mode='stretch_width', dynamic=True)
+
+        self.main = pn.Column(self.tabs, sizing_mode='stretch_width')
 
     def servable(self):
         self.main.servable(target="main")
