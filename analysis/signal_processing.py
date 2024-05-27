cfccc4559e42573a0796dbd046b7b9f9a99ae312
Ramz <ramzalqasir@web.de>
2024-05-27 22:10:16 +0200

    Added signal_processing.py again.

---
+++ analysis/signal_processing.py
@@ -0,0 +1,13 @@
+# Hier kommen unsere Funktionen wie Leistungsdichtespektrum, Korrelation etc.
+class Signal_process:
+
+    def __init__(self,signal1, signal2):
+
+        self.signal1 = signal1
+        self.signal2 = signal2
+
+    def Funktionalität1(self):
+        return None
+
+    def Funktionalität2(self):
+        return None
