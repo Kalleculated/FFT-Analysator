27897a2c6ddc71307b4c76f9824879ca81a68b67
SewitMeles <sewit_meles@hotmail.de>
2024-05-27 14:59:27 +0000

    Test von Sewit

--- analysis/preprocessing.py
+++ analysis/preprocessing.py
@@ -33,6 +33,13 @@ class Preprocess:
     def get_abtastrate(self):
         # Muss noch befüllt werden. Vermutlich bietet Acourlar hier eine Funktion oder es kann aus dem Datenset gelesen
         # gelesen werden
-        return None
+        return "test"
     
-    print("test")
+
+
+
+
+
+print(Preprocess.get_abtastrate(Preprocess))
+
+
