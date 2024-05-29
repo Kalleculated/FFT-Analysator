88a2705440d9f4ada95bb227c37292d7450fa7ec
kalleculated <pascal.kisin@gmail.com>
2024-05-29 12:00:28 +0200

    moved app into src/fft-analysator for better docs generator later on

--- src/fft-analysator/app.py
+++ src/fft-analysator/app.py
@@ -3,7 +3,7 @@ from gui.controllers.app_controller import AppController
 class App:
     def __init__(self):
         self.app = AppController()
-    
+
     def run(self):
         self.app.servable()
 
