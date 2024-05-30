d1b6333ec42cf03369c8b08043432801c3f1c51b
Maxxxxi <64035257+Maxxxxi@users.noreply.github.com>
2024-05-30 19:27:50 +0200

    Chose color of the channels and move them seperatly

---
+++ src/fft_analysator/gui/components/color_picker.py
@@ -0,0 +1,11 @@
+import panel as pn
+
+
+class Colorpicker:
+    def __init__(self):
+        self.color_picker = pn.widgets.ColorPicker
+        self._component = self.color_picker(name='', value='#FF0000', margin=(10, 15))
+
+    @property
+    def component(self):
+        return self._component
