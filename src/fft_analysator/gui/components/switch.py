14c7cb9a3261fe3d91a535ef0ef3b07f53ca3cac
kalleculated <pascal.kisin@gmail.com>
2024-06-01 17:04:24 +0200

    gave the switch some height

--- src/fft_analysator/gui/components/switch.py
+++ src/fft_analysator/gui/components/switch.py
@@ -4,7 +4,7 @@ import panel as pn
 class Switch:
     def __init__(self):
         self.switch = pn.widgets.Switch
-        self._component = self.switch(name='test', width=30, margin=(0, 165))
+        self._component = self.switch(name='test', width=30, margin=(0, 165), height=30)
 
     @property
     def component(self):
