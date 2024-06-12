22331ac5dba4f51a989d48887c5c1dd2c1bf88cd
SewitMeles <sewit_meles@hotmail.de>
2024-06-12 03:41:52 +0200

    FFT und power spectrum without Komposition

--- src/fft_analysator/analysis/signal_processing.py
+++ src/fft_analysator/analysis/signal_processing.py
@@ -1,20 +1,38 @@
 # Hier kommen unsere Funktionen wie Leistungsdichtespektrum, Korrelation etc.
-import os
-
-import numpy
+import acoular as ac
 import numpy as np
 import fft_analysator.analysis.preprocessing as pp
-import scipy as sc
 import matplotlib.pyplot as plt
-import acoular as ac
-class Signal_process:
-    def __init__(self,data):
-        self.current_data = data
-    def FFT(self):
-        #plt.plot(data_fft2.fftfreq(),ft[:,0])
-        #plt.show()
-        ac.spectra
-        return sc.fft(self.current_data)
 
+
+class Signa_lAnalyzer:
+    def __init__(self, file_paths, block_size=512):
+        self.preprocess = pp.Preprocess(file_paths=file_paths, block_size=block_size)
+
+    def set_current_channel(self, channel):
+        self.preprocess.set_current_channel(channel)
+
+    def calculate_fft(self):
+        self.preprocess.set_channel_data()  
+        channel_data = self.preprocess.selected_channel_data
+        fft_result = fft.fft(channel_data)
+        freqs = fft.fftfreq(len(channel_data))
+        return freqs, np.abs(fft_result)
+
+    def calculate_power_spectrum(self):
+        freqs, fft_result = self.calculate_fft()
+        power_spectrum = np.square(np.abs(fft_result))
+        return freqs, power_spectrum
+
+
+    def plot_fft(self):
+        freqs, fft_result = self.calculate_fft()
+        plt.figure(figsize=(10, 6))
+        plt.plot(freqs, fft_result)
+        plt.title("FFT of Channel")
+        plt.xlabel("Frequency (Hz)")
+        plt.ylabel("Magnitude")
+        plt.grid(True)
+        plt.show()
 
 
