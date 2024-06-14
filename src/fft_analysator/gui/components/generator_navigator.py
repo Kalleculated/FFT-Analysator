3623151f33828f17a2e58a7b250955806e5c3018
kalleculated <pascal.kisin@gmail.com>
2024-06-14 22:33:27 +0200

    added index logic

--- src/fft_analysator/gui/components/generator_navigator.py
+++ src/fft_analysator/gui/components/generator_navigator.py
@@ -6,7 +6,7 @@ class GeneratorNavigator:
         self.button_back = pn.widgets.Button(name='\u25c0', button_type='default', margin=(2, 5, 5, 5), disabled=True)
         self.button_forward = pn.widgets.Button(name='\u25b6', button_type='default', margin=(2, 5, 5, 5), disabled=True)
         self.reset_button = pn.widgets.Button(name='\u21ba', button_type='default', margin=(2, 5, 5, 5), disabled=True)
-        self.index_box = pn.widgets.IntInput(name='Block index:', value=0, margin=(-20, 20, 0, 10), disabled=True, width=80)
+        self.index_box = pn.widgets.IntInput(name='Index:', value=0, margin=(-20, 20, 0, 10), disabled=True, width=80)
         self.goto_button = pn.widgets.Button(name='Go', button_type='default', margin=(2, 5, 5, 5), disabled=True)
         self._component = pn.Row(self.goto_button, self.index_box, self.button_back, self.reset_button, self.button_forward,
                                  margin=(5, 5, 10, 5))
