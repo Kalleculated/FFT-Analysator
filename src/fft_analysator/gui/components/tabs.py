941e5eba9987d67c1465c95f85e6c49579855a07
Maxxxxi <64035257+Maxxxxi@users.noreply.github.com>
2024-06-14 18:34:39 +0200

    Add Frequenzgang Plots, dynamic timeaxis

--- src/fft_analysator/gui/components/tabs.py
+++ src/fft_analysator/gui/components/tabs.py
@@ -5,7 +5,7 @@ class Tabs:
     def __init__(self):
         self.tabs = pn.Tabs
         self.str_signal_tab = "Signalinput"
-        self.str_Spektrum_tab = "Spektrum"
+        self.str_Spektrum_tab = "Frequenzgang"
         self.str_Impulsantwort_tab = "Impulsantwort"
         self.str_Analysefuktionen_tab = "Analysefunktionen"
         self._component = self.tabs(
