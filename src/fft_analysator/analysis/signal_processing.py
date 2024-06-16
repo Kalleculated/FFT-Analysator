76e04d497f3bf3ed7bd2c6c6902a34149070c080
Ramz <ramzalqasir@web.de>
2024-06-16 18:38:35 +0200

    Added correlation function and Cross Spectral density

--- src/fft_analysator/analysis/signal_processing.py
+++ src/fft_analysator/analysis/signal_processing.py
@@ -1,5 +1,6 @@
 # Hier kommen unsere Funktionen wie Leistungsdichtespektrum, Korrelation etc.
 import numpy as np
+import scipy.fft
 import scipy.fft as fft
 import matplotlib.pyplot as plt
 import scipy.signal as ss
@@ -7,21 +8,26 @@ import scipy.signal as ss
 
 class SignalProcess:
     def __init__(self, data):
-        self.current_data = data
+        self.current_data = data # Muss angepasst werden bzw. wird eigentlich nicht benötigt.
 
-    def calculate_fft(self):
+    def calculate_fft(self, signal):
         # Perform the FFT
-        fft_result = np.fft.fft(self.current_data)
-        freqs = np.fft.fftfreq(len(self.current_data))
+        fft_result = np.fft.fft(signal)
+        freqs = np.fft.fftfreq(len(signal))
         return fft_result, freqs
 
-    def calculate_power_spectrum(self):
-        # Calculate the power spectrum
-        #fft_result = np.fft.fft(self.current_data)
-        #power_spectrum = np.square(np.abs(fft_result))
-        freqs , power_spectrum = ss.welch(self.current_data, window= "hann", nperseg =512 )
-        return  freqs, power_spectrum
+    def calculate_cross_spectrum(self, signal_x, signal_y, window="hann", seg_length=1024, overlap=512):
+        # Calculates the cross power spectra density of two signals.
+        # If x and y are the same signal, this will calculate instead the power spectral density of this signal
+        f, Pxy = ss.csd(x=signal_x, y=signal_y, window=window, nperseg=seg_length, noverlap=overlap)
 
+        return f, Pxy
+
+    def calculate_correlation(self, signal_x, signal_y):
+        return ss.correlate(signal_x, signal_y)
+
+
+"""
     def plot_power_spectrum(self, power_spectrum, freqs):
         plt.figure(figsize=(10, 6))
         plt.semilogy(freqs, power_spectrum)
@@ -30,7 +36,7 @@ class SignalProcess:
         plt.ylabel("Power")
         plt.grid(True)
         plt.show()
-        
+
     def plot_fft(self):
         fft_result, freqs = self.calculate_fft()
         plt.figure(figsize=(10, 6))
@@ -40,7 +46,7 @@ class SignalProcess:
         plt.ylabel("Magnitude")
         plt.grid(True)
         plt.show()
-    
+
 
 # Beispielhafte Verwendung mit der Preprocess-Klasse
 #file_paths = "three_sources.h5"
@@ -57,6 +63,4 @@ class SignalProcess:
 #signal_processor.plot_power_spectrum(power_spectrum, freqs)
 
 
-
-
-
+"""
