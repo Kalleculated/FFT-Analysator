402335b7861a85ad241573f3fb966eb4493ef6ac
SewitMeles <sewit_meles@hotmail.de>
2024-06-19 22:16:23 +0200

    Mit input und output data

--- src/fft_analysator/analysis/signal_processing.py
+++ src/fft_analysator/analysis/signal_processing.py
@@ -1,64 +1,53 @@
 # Hier kommen unsere Funktionen wie Leistungsdichtespektrum, Korrelation etc.
 import numpy as np
-import scipy.fft
 import scipy.fft as fft
 import matplotlib.pyplot as plt
 import scipy.signal as ss
 
 
-class SignalProcess:
-    def __init__(self, data):
-        self.current_data = data # Muss angepasst werden bzw. wird eigentlich nicht benötigt.
+class Signal_Process:
+    def __init__(self, data1, data2, abtastrate):
+        self.current_data_input = data1
+        self.current_data_output = data2
+        self.current_data_abtastrate = abtastrate
 
-    def calculate_fft(self):
-        fft_result = fft.rfft(self.current_data)
-        freqs = fft.rfftfreq(len(self.current_data))  
+    def calculate_fft(self, data):
+        fft_result = fft.rfft(data)
+        freqs = fft.rfftfreq(len(data), d=1/self.current_data_abtastrate)
+        return freqs, fft_result
 
-    def calculate_cross_spectrum(self, signal_x, signal_y, window="hann", seg_length=1024, overlap=512):
-        # Calculates the cross power spectra density of two signals.
-        # If x and y are the same signal, this will calculate instead the power spectral density of this signal
-        f, Pxy = ss.csd(x=signal_x, y=signal_y, window=window, nperseg=seg_length, noverlap=overlap)
-
+    def calculate_cross_spectrum(self, window="hann", seg_length=1024, overlap=512):
+        f, Pxy = ss.csd(x=self.current_data_input, y=self.current_data_output, window=window, nperseg=seg_length, noverlap=overlap)
         return f, Pxy
 
-    def calculate_correlation(self, signal_x, signal_y):
-        return ss.correlate(signal_x, signal_y)
+
+    def calculate_correlation(self):
+        corr = ss.correlate(self.current_data_input, self.current_data_output, mode='same')
+        return corr
+
+    def calculate_impulse_response(self):
+        freq_x, FFT_X = self.calculate_fft(self.current_data_input)
+        freq_y, FFT_Y = self.calculate_fft(self.current_data_output)
+        H = FFT_X / FFT_Y 
+        h = fft.irfft(H, n=len(self.current_data_input))
+        N = len(self.current_data_input)
+        time_axis = np.arange(N) / self.current_data_abtastrate 
+        return time_axis, h
 
 
-"""
-    def plot_power_spectrum(self, power_spectrum, freqs):
-        plt.figure(figsize=(10, 6))
-        plt.semilogy(freqs, power_spectrum)
-        plt.title("Power Spectrum of the Signal")
-        plt.xlabel("Frequency (Hz)")
-        plt.ylabel("Power")
-        plt.grid(True)
-        plt.show()
-
-    def plot_fft(self):
-        fft_result, freqs = self.calculate_fft()
-        plt.figure(figsize=(10, 6))
-        plt.plot(freqs, np.abs(fft_result))
-        plt.title("FFT of Channel")
-        plt.xlabel("Frequency (Hz)")
-        plt.ylabel("Magnitude")
-        plt.grid(True)
-        plt.show()
+    def calculate_frequency_response(self):
+        freq_x, FFT_X = self.calculate_fft(self.current_data_input)
+        freq_y, FFT_Y = self.calculate_fft(self.current_data_output)
+        H = FFT_X / FFT_Y 
+        return freq_x, H
 
 
-# Beispielhafte Verwendung mit der Preprocess-Klasse
-#file_paths = "three_sources.h5"
-#preprocessor = Preprocess(file_paths=file_paths, block_size=1024)
-#preprocessor.set_current_channel(10)  # Setze den aktuellen Kanal auf Kanal 10
-#preprocessor.set_channel_data()      # Sammle die Daten des aktuellen Kanals
-
-# Erstellen einer Instanz von SignalProcess und FFT durchführen
-#signal_processor = SignalProcess(preprocessor.selected_channel_data)
-#signal_processor.plot_fft()
 
 
-# Plotten des Leistungsdichtespektrums
-#signal_processor.plot_power_spectrum(power_spectrum, freqs)
 
 
-"""
+
+
+
+
+
