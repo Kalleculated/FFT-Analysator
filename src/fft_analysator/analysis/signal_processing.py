04e52c624f385f71cf1c92559330b4b54789e33a
Ramz <ramzalqasir@web.de>
2024-06-20 14:22:39 +0200

    Fixed some notation

--- src/fft_analysator/analysis/signal_processing.py
+++ src/fft_analysator/analysis/signal_processing.py
@@ -16,19 +16,19 @@ class Signal_Process:
         return freqs, fft_result
 
     def calculate_cross_spectrum(self, window="hann", seg_length=1024, overlap=512):
-        f, Pxy = ss.csd(x=self.current_data_input, y=self.current_data_output, window=window, nperseg=seg_length, noverlap=overlap)
+        f, Pxy = sc.csd(x=self.current_data_input, y=self.current_data_output, window=window, nperseg=seg_length, noverlap=overlap)
         return f, Pxy
 
 
     def calculate_correlation(self):
-        corr = ss.correlate(self.current_data_input, self.current_data_output, mode='same')
+        corr = sc.correlate(self.current_data_input, self.current_data_output, mode='same')
         return corr
-    
+
     def calculate_power_spectrum(self,data, window= "hann", nperseg =512 ):
         freqs , power_spectrum = sc.welch(data, window= "hann", nperseg =512 )
         return  freqs, power_spectrum
-    
-    
+
+
     def calculate_coherence(self, window='hann', seg_length=1024, overlap=512):
         f, Cxy = sc.coherence(self.current_data_input, self.current_data_output, fs=self.current_data_abtastrate, window=window, nperseg=seg_length, noverlap=overlap)
         return f, Cxy
@@ -36,10 +36,10 @@ class Signal_Process:
     def calculate_impulse_response(self):
         freq_x, FFT_X = self.calculate_fft(self.current_data_input)
         freq_y, FFT_Y = self.calculate_fft(self.current_data_output)
-        H = np.divide(FFT_X, FFT_Y, out=np.zeros_like(FFT_X), where=(np.abs(FFT_Y) > 1e-10)) 
+        H = np.divide(FFT_X, FFT_Y, out=np.zeros_like(FFT_X), where=(np.abs(FFT_Y) > 1e-10))
         h = fft.irfft(H, n=len(self.current_data_input))
         N = len(self.current_data_input)
-        time_axis = np.arange(N) / self.current_data_abtastrate 
+        time_axis = np.arange(N) / self.current_data_abtastrate
         return time_axis, h
 
 
