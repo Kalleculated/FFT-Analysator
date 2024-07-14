30e815463ffb4057f237742ec6aa844c6085fbb8
Maxxxxi <64035257+Maxxxxi@users.noreply.github.com>
2024-07-15 00:34:06 +0200

    adjust_some ylabel

--- src/fft_analysator/analysis/plotting.py
+++ src/fft_analysator/analysis/plotting.py
@@ -101,11 +101,11 @@ class Plotter:
                 # Get the time_data block wise for the given channel
                 time_data = self.data_callback.set_channel_on_data_block(channel)
                 scale_min_factor = 0.2 # set min scale factor
-                y_label = "Sound Pressure in Pa"
+                y_label = r'Sound Pressure in $$\mathrm{Pa}$$'
             else:
                 time_data = self.signal_process.SPL(channel)
                 scale_min_factor = -0.2 # set min scale factor
-                y_label = "SPL in dB" # "Sound Pressure Level
+                y_label = r'$$\mathrm{L_{p}}$$ in $$\mathrm{dB}$$' # SPL
 
             # set max scale factor
             scale_max_factor = 0.2
@@ -168,10 +168,10 @@ class Plotter:
         # values in dB
         if not self.db:
             H =  self.signal_process.frequency_response(frq_rsp_dB=False)
-            y_label = "Sound Pressure in Pa"
+            y_label = r'Sound Pressure in $$\mathrm{Pa}$$'
         else:
             H =  self.signal_process.frequency_response(frq_rsp_dB=True)
-            y_label = "SPL(f) in dB" # "Sound Pressure Level
+            y_label = r'$$\mathrm{L_{p}}$$ in $$\mathrm{dB}$$'# SPL
 
         # Create the phase response
         phi = self.signal_process.phase_response(deg=True)
@@ -211,11 +211,11 @@ class Plotter:
         if not self.db:
             h = self.signal_process.impuls_response(imp_dB=False).real
             scale_min_factor = 0.2 # set min scale factor
-            y_label = "Sound Pressure in Pa"
+            y_label = r'Sound Pressure in $$\mathrm{Pa}$$'
         else:
             h = self.signal_process.impuls_response(imp_dB=True).real
             scale_min_factor = -0.2 # set min scale factor
-            y_label = "SPL in dB" # "Sound Pressure Level
+            y_label = r'$$\mathrm{L_{p}}$$ in $$\mathrm{dB}$$'# SPL
 
         # set max scale factor
         scale_max_factor = 0.2
