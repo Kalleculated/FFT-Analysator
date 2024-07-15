c2a20138824bb813b1f992eacd2ecfef94c826f7
Ramz <ramzalqasir@web.de>
2024-07-15 03:46:24 +0200

    Updated DocStrings

--- src/fft_analysator/analysis/preprocessing.py
+++ src/fft_analysator/analysis/preprocessing.py
@@ -9,12 +9,38 @@ class Preprocess:
     internal Signalprocessing class. It contains information about the block size, data, channel count and size and
     sample frequency. It is able to return the complete data as a Numpy array or iterate over the defined block size.
 
-    Args:
-        file_paths (object): Get the callback to the data object
-        block_size (int): Length of data block.
-    """
+    Attributes:
+        file_paths (string or path): A String or Path to import.
+        block_size (int): Block size for processing.
+        table_key (string): Table key names.
+        source (Acoular.TimeSamples): Acoular TimeSamples class.
+        source_result (generator): Generator of the current block result.
+        selected_data_block (Numpy Array): Selected data block.
+        current_block_idx (int): Index of the current data block.
+        selected_channel_data (Numpy Array): All data of selected channel.
 
+
+    Methods:
+        reinitialize_source(): Reinitializes the generator
+        set_channel_data(int): Gives the complete data of a channel
+        set_channel_on_data_block(int): Gives the data block of a channel
+        set_next_data_block(): Gives the next data block
+        set_current_channel(int): Changes the current selected channel
+        set_data_block_to_idx(int): Returns the data block of an idx
+        get_channel_size(): Gets the channel size
+        get_channel_count(): Gets the channel count
+        get_abtastrate(): Gets the sample frequency
+        get_table_names(): Gets the table names
+    """
     def __init__(self, file_paths=None, block_size=1024):
+        """
+        Constructs all the necessary attributes for the Preprocess object.
+
+
+        Args:
+            file_paths (object): Get the callback to the data object
+            block_size (int): Length of data block.
+        """
 
         self.file_paths = file_paths
         self.block_size = block_size
@@ -26,9 +52,7 @@ class Preprocess:
             self.selected_data_block = next(self.source_result)
 
         self.current_block_idx = 0
-        self.data = None
-        self.channel_count = None
-        self.channel_size = None
+
         self.selected_channel_data = np.array([])
 
     def reinitialize_source(self):
@@ -61,6 +85,8 @@ class Preprocess:
 
         Args:
             channel (int): Channel number.
+        Returns:
+            Numpy Array
         """
         return self.selected_data_block[:, channel]
 
@@ -103,6 +129,9 @@ class Preprocess:
     def get_channel_size(self):
         """
         Get_channel_size returns the size of the current channel
+
+        Returns:
+            size (int): Channel size
         """
         #size = np.array(self.source)[:, self.current_channel].shape[0]
         size = np.array(self.converted_file)[:, self.current_channel].shape[0]
@@ -111,13 +140,20 @@ class Preprocess:
     def get_channel_count(self):
         """
         Get_channel_count returns the number of channels of the current .h5 file
+
+        Returns:
+            count (int): Number of channels
         """
         count = self.source.numchannels
+
         return count
 
     def get_abtastrate(self):
         """
         Get_abtastrate returns the sample_freq from the current .h5 file via Acoular
+
+        Returns:
+            abtasterate (int): Sample Frequency
         """
         abtastrate = self.source.sample_freq
         return abtastrate
@@ -125,9 +161,13 @@ class Preprocess:
     def get_table_names(self):
         """
         Get_table_names returns a list of all data set keyword contained in the current .h5 File
+
+        Returns:
+            keys (list): A list of table names
         """
         with h5py.File(self.file_paths, 'r') as file:
             # Zugriff auf den gewünschten Datensatz
             keys = list(file.keys())
+
 
         return keys
